e5e27261fcd6204c6e07cb8da034a7fb6f334440
Lucas-Peng-CS <cs20210310@gmail.com>
2021-08-08 22:06:18 +0800

    feat: add user authentication in restaurants and home routers

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost/restaurant-list", {
+mongoose.connect('mongodb://localhost/restaurant-list', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  useUnifiedTopology: true
+})
 
 const db = mongoose.connection
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 module.exports = db
